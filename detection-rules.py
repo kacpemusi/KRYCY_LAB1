75475c7b63fbf811e29dc0b55425824548150c51
DaftyZ <lemureg@gmail.com>
2023-01-05 22:17:13 +0100

    jak wyzej

--- detection-rules.py
+++ detection-rules.py
@@ -1,3 +1,14 @@
 #reguły analityczne uzywane do detekcji zdarzen
 #przekazywane do scannera
 #OFF.DETPY.1.1 OFF.DETPY.1.2
+
+import datetime
+
+def example_rule(event):
+    if event['method'] == 'POST' and event['status_code'] == '200': #and (datetime.datetime.fromisoformat(event['ts']).hour > 15 or datetime.datetime.fromisoformat(event['ts']).hour < 8):
+        action_alert = "remote"
+        description = "Alert: DN"
+    else:
+        action_alert = None
+        description = None
+    return action_alert, description
--- sigma_handler.py
+++ sigma_handler.py
@@ -3,3 +3,23 @@
 #obsluga bledow ze sciezka itp
 #REG.DET.1
 #REG.DET.1.2
+
+def read_rules_from_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
+
+def parse_rules(rules_string):
+    return sigmac.parse(rules_string)
+
+def execute_rules(parsed_rules, event_data):
+    results = []
+    for rule in parsed_rules:
+        result = rule.execute(event=event_data)
+        results.append(result)
+    return results
+
+def handle_rules(file_path, event_data):
+    rules_string = read_rules_from_file(file_path)
+    parsed_rules = parse_rules(rules_string)
+    results = execute_rules(parsed_rules, event_data)
+    return results
