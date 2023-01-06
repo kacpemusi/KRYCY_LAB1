0805d010b3abd0fa68c69f798fc4a3326e4803f7
DaftyZ <lemureg@gmail.com>
2023-01-06 22:46:04 +0100

    dziala agent 8)

--- app/agent.py
+++ app/agent.py
@@ -1,3 +1,28 @@
-#aplikacja glowna agenta w trybie CLI
-#obsluguje po prostu uzytkownika
-#GEN.MGMT.6 i 6.1, GEN.AGT.1 ,
+#komunikuje sie z glowny klientem po REST API
+#moze:
+#przekazac glownemu hostowi informacje o swojej konfiguracji
+#przechwytywac ruch sieciowy i zapisac go do pliku .pcap
+#przekazac za pomoca JSONA konfiguracje zbierania
+#pobieranie i przekazywanie plikow na glownego klienta
+#aplikacja centralna moze na nim wykonac polecenia systemowe
+#komunikacja z alertami logami itp z glownym hostem
+#całe ON.REM
+
+from flask import Flask, request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/api/execute', methods=['POST'])
+def execute():
+    # Get the command from the request payload
+    command = request.get_json()['command']
+
+    # Execute the command and get the output
+    output = subprocess.run(command, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    # Return the output as a response
+    return jsonify({'output': output})
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
---
+++ app/agent_command.py
@@ -0,0 +1,24 @@
+#aplikacja glowna agenta w trybie CLI
+#obsluguje po prostu uzytkownika
+#GEN.MGMT.6 i 6.1, GEN.AGT.1 ,
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: agent_command.py COMMAND")
+
+# Set the API endpoint URL
+url = 'http://localhost:5000/api/execute'
+
+# Set the command to be executed
+command = sys.argv[1]
+
+# Send the request with the command as the payload
+response = requests.post(url, json={'command': command})
+
+# Get the output from the response
+output = response.json()['output']
+
+# Print the output
+print(output)
